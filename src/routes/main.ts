1a6dcdc402fe36271ecbcb0ae05c36a20c9bb5eb
Leomar Nogueira <leomar.nogueira-extern@renault.com>
2023-01-02 19:50:15 -0300

    Separar routes do arquivo main

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,72 +1,15 @@
-import { PrismaClient } from '@prisma/client';
 import { Router } from 'express';
-
-const routes = Router();
-const prisma = new PrismaClient();
-
-routes.post('/products', async (req, res) => {
-  const { author, title, price } = req.body;
-
-  const book = await prisma.books.create({
-    data: {
-      author,
-      title,
-      price
-    }
-  })
-  return res.status(201).json(book);
-});
-
-routes.get('/products', async (req, res) => {
-  const findAllBooks = await prisma.books.findMany({ orderBy: { id: 'asc' } });
-  return res.status(200).json(findAllBooks);
-});
-
-routes.put('/products/:id', async (req, res) => {
-  const id = parseInt(req.params.id);
-  const { author, title, price } = req.body;
+import creatBooks from './create-books.routes';
+import deleteBooks from './delete-books.routes';
+import readBooks from './read-books.routes';
+import updateBooks from './update-books.routes';
 
 
-  if (!id) {
-    return res.status(400).json('deve conter um id');
-  }
+const routes = Router()
 
-  const idFind = await prisma.books.findUnique({ where: { id } });
-
-  if (!idFind) {
-    return res.status(404).json('deve conter id válido');
-  }
-
-  const books = await prisma.books.update({
-    where: {
-      id,
-    },
-    data: {
-      author,
-      title,
-      price
-    },
-  });
-
-  return res.status(200).json(books);
-});
-
-routes.delete('/products/:id', async (req, res) => {
-  const id = parseInt(req.params.id);
-
-  if (!id) {
-    return res.status(400).json('dever conter um id');
-  }
-
-  const idFind = await prisma.books.findUnique({ where: { id } });
-
-  if (!idFind) {
-    return res.status(404).json('deve conter id válido ');
-  }
-
-  await prisma.books.delete({ where: { id } });
-
-  return res.status(200).send('Produto deletado com sucesso!')
-});
+routes.use('/products', creatBooks)
+routes.use('/products', readBooks)
+routes.use('/products', updateBooks)
+routes.use('/products', deleteBooks)
 
 export default routes;
